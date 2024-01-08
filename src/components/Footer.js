bf0c60e16f289f87fd9c4e96db9f071d64069e67
Gaurav0111 <garwalgaurav1@gmail.com>
2024-01-08 20:01:47 +0530

    added footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,36 @@
 // src/components/Footer.js
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Footer.css';
 
 function Footer() {
+  const [feedback, setFeedback] = useState('');
+
+  const handleFeedbackSubmit = () => {
+    if (feedback.trim() === '') {
+      alert('Please provide feedback before submitting.');
+    } else {
+      alert('Thank you for your feedback!');
+    }
+  };
+
   return (
     <footer className="footer">
       <div className="feedback">
         <p>Send Feedback Now</p>
-        <input type="email" placeholder="Your Email" />
+        <input
+          type="text"
+          placeholder="feedback"
+          value={feedback}
+          onChange={(e) => setFeedback(e.target.value)}
+        />
+        <button className='footer-button' onClick={handleFeedbackSubmit}>Submit</button>
       </div>
-      <h1>Gaurav Garwal</h1>
       <div className="footer-text">
+      <h1>Gaurav Garwal</h1>
         <p>&copy; {new Date().getFullYear()} Gaurav Garwal | All Rights Reserved</p>
         <p className='parara' >Email: garwalgaurav480@gmail.com</p>
+      </div>
+      <div>
         <p className="social-links">
           <a href="https://www.instagram.com/gaurav.garwal/" target="_blank" rel="noopener noreferrer">
             Instagram
